f1673775b940531a587e553c1cd371c5310b8836
GamingX <GamingX@posteo.de>
2025-04-09 16:54:58 +0200

    batch file
    Signed-off-by: GamingX <GamingX@posteo.de>

---
+++ bvd.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal
+
+set SCRIPT_DIR=%~dp0
+set VENV_DIR=%SCRIPT_DIR%venv
+set PYTHON_EXE=%VENV_DIR%\Scripts\python.exe
+set REQUIREMENTS=%SCRIPT_DIR%\src\requirements.txt
+set SCRIPT=%SCRIPT_DIR%\src\main.py
+
+set PATH=%PATH%;%SCRIPT_DIR%
+
+setx PATH "%PATH%;%SCRIPT_DIR%"
+
+if not exist "%PYTHON_EXE%" (
+    echo Creating virtual environment...
+    python -m venv "%VENV_DIR%"
+)
+
+"%PYTHON_EXE%" -m pip install --upgrade pip >nul
+if exist "%REQUIREMENTS%" (
+    "%PYTHON_EXE%" -m pip install -r "%REQUIREMENTS%" -q
+) else (
+    echo No requirements.txt found.
+    exit /b 1
+)
+
+"%PYTHON_EXE%" "%SCRIPT%" %*
+
+endlocal
